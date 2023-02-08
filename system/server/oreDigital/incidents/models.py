49e45fd8f3a5fa96a7f828648ab524474bd6de35
ishimwe-emma <ishimweemmanuel2005@gmail.com>
2023-02-08 13:12:24 +0200

    [mod]: changed the model status field

--- system/server/oreDigital/incidents/models.py
+++ system/server/oreDigital/incidents/models.py
@@ -4,11 +4,11 @@ from model_utils import Choices
 # Create your models here.
 
 class Incident(models.Model):
-    STATUS = Choices('normal', 'low', 'high')
+    STATUS = Choices('low', 'high')
     INCIDENT_TYPE = Choices('landslide', 'temperature')
     incidentId = models.UUIDField()
     description = models.CharField(max_length = 200)
-    status = models.CharField(choices=STATUS, default=STATUS.normal, max_length = 10)
+    status = models.CharField(choices=STATUS, default=STATUS.low, max_length = 10)
     incidentType = models.CharField(choices=INCIDENT_TYPE, default=INCIDENT_TYPE.temperature, max_length = 20)
     measurement = models.DecimalField(max_digits=10, decimal_places=10)
     date = models.DateField(auto_now=True)
